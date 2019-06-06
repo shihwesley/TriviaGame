704e7570a5bd9572b7f2b8e460d5ad37c5846238
Wesley Shih <shih.wesley@gmail.com>
2019-06-06 12:45:22 -0700

    finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ var questions = ["What date does Marty first travel to in the DeLorean?",
                 ];
 var answers = ['November 5th 1955',
                 'Breeding Pine Trees',
-                '$80',
+                '80',
                 'A Match Made in Space',
                 '7:53am'
                 ];
@@ -15,19 +15,20 @@ var correct = ['a', 'c', 'c', 'd', 'b'];
 window.onload = function() {
     $("#reset").on("click", reset);
     $("#start").on("click", start);
+    $("#submit").on("click", submit);
 };
 
 var intervalId;
 var clockRunning = false;
 var time = 120;
 var score = 0;
+var txt = [];
 
 function reset() {
     time = 120;
     stop();
     // DONE: Change the "display" div to "00:00."
     $("#time-left").text("120");
-
 }
 
 function start() {
@@ -37,7 +38,7 @@ function start() {
         intervalId = setInterval(count, 1000);
         clockRunning = true;
     }
-    choosing();
+
 }
 
 function stop() {
@@ -53,6 +54,7 @@ function count() {
     time--;
     $("#time-left").text(time);
     if (time === 0) {
+        comparing();
         alert("Time is Up !");
         reset();
         $('.questions').remove();
@@ -60,25 +62,52 @@ function count() {
     }
 }
 
-function choosing() {
-    console.log( $(':checkbox').val(this.checked));
 
-    var checkbox = document.getElementsByName('type');
-    console.log(checkbox);
-    //if (checkbox[0].checked) {
-       // alert('checked');
-    //} else {
-        //lert('did not check');
-    //}
+function comparing() {
+    var coffee = document.forms[0];
+    var testing = coffee['q1-op1'].checked;
+    var omitted = 0;
+    console.log(testing);
 
+        for (var k = 1; k < 6; k++) {
+            for (var l = 1; l< 5; l++) {
+                var testing = coffee['q'+k + "-op"+l].checked;
+                if (testing) {
+                    txt.push(coffee['q'+k + "-op"+l].value);
+                } else {
+                    omitted++;
+                    console.log("inside else: " + omitted);
+                    if (omitted === 4) {
+                        txt.push('omit');
+                    }
+                }
+            }
+            omitted = 0;
+        }
+        console.log(txt);
+    if (txt.length > answers.length) {
+        alert(" You have chosen more then 5 answers!");
+    }
+    for (var j =0; j < answers.length;j++) {
+        if (answers[j]===txt[j]) {
+            score++;
+        }
+    }
+}
+
+function submit () {
+    comparing();
+    stop();
+    $('.questions').remove();
+    results();
 }
 
 
 function results() {
-    //$('.jumbotron').append('<h1> You Scored: ' + score);
+    $('.jumbotron').append('<h1> You Scored: ' + score);
     $('.jumbotron').append('<p> 1. November 5th 1955');
     $('.jumbotron').append('<p> 2. Breeding Pine Trees');
-    $('.jumbotron').append('<p> 3. $80');
+    $('.jumbotron').append('<p> 3. 80');
     $('.jumbotron').append('<p> 4. A Match Made in Space');
     $('.jumbotron').append('<p> 5. 7:53am');
 }
